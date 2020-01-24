1d11410bfa09a1d5c824b14a5b3ca1a415939f0e
David Burhans <david.burhans@gmail.com>
2020-01-23 23:26:04 -0600

    fix gravity. generate entire forest. add tree branches. make trees cones for better shadows

--- Player.gd
+++ Player.gd
@@ -1,16 +1,17 @@
-extends Spatial
+extends KinematicBody
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var velocity = Vector3.ZERO
 var speed = 5
+var g = -9.8
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
 	elif Input.is_action_pressed("ui_right"):
@@ -23,5 +24,6 @@ func _process(delta):
 		velocity.z = speed
 	else:
 		velocity.z = 0
-	
-	velocity = $KinematicBody.move_and_slide(velocity)
+		
+	velocity.y += g * delta
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
--- Tree.gd
+++ Tree.gd
@@ -1,14 +1,14 @@
-extends Spatial
+extends StaticBody
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-onready var trunk = get_node("KinematicBody/Trunk")
+onready var trunk = get_node("Trunk")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(trunk)
+	trunk.rotate_y(randi() % 360)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,4 +16,19 @@ func _ready():
 #	pass
 
 func _grow_branch(trnk):
+	var branch = trunk.duplicate()
+	var shape = branch.get_child(0)
+	var new_radius = shape.radius / 4
+	var new_height = shape.height / 4
+	var height_diff = shape.height - new_height
+	print(new_height, ", ", new_radius)
+	# var rot_x = randi() % 45 + 20
+	var rot_z = 45
+	var x_translate = cos(deg2rad(rot_z)) * new_height	
+	shape.radius = new_radius
+	shape.height = new_height	
+	#branch.rotate_x(rot_x)
+	branch.rotate_z(rot_z)
+	branch.translate(Vector3(-x_translate, 0, 0))	
+	trnk.add_child(branch)
 	pass
