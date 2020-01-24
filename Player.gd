12d0f1906a037ee290c10c375759614a02307334
David Burhans <david.burhans@gmail.com>
2020-01-24 08:47:04 -0600

    adjust camera and lighting to look more like poc. add janky footprints

--- Player.gd
+++ Player.gd
@@ -6,6 +6,9 @@ extends KinematicBody
 var velocity = Vector3.ZERO
 var speed = 5
 var g = -9.8
+var last_print = Vector3.ZERO
+onready var footprints = get_node("Footprints")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -24,6 +27,12 @@ func _physics_process(delta):
 		velocity.z = speed
 	else:
 		velocity.z = 0
+	
+	if translation.distance_to(last_print) > 1.0:		
+		footprints.emitting = true
+		last_print = translation
+
 		
+	
 	velocity.y += g * delta
 	velocity = move_and_slide(velocity, Vector3(0,1,0))
